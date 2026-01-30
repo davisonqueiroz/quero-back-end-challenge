dfced1f8972918434aea83d257d869d7915a8aa3
davison queiroz <davisonvinicius@outlook.com.br>
2026-01-29 21:38:04 -0300

    fix in use of enums

--- offers-challenge/app/models/offer.rb
+++ offers-challenge/app/models/offer.rb
@@ -2,8 +2,15 @@ class Offer < ApplicationRecord
   validates :course_name, :rating, :full_price, :offered_price, :kind, :level, :ies_logo, :ies_name, presence: true
   validates :full_price, :offered_price, numericality: { greater_than_or_equal_to: 0 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  enum kind: { presencial: 'presencial', ead: 'ead'  }
-  enum level: { tecnologo: 'tecnologo', licenciatura: 'licenciatura', bacharelado: 'bacharelado' } 
+  enum :kind, {
+    presencial: 'presencial',
+    ead: 'ead'  
+  }
+  enum :level, {
+    tecnologo: 'tecnologo',
+    licenciatura: 'licenciatura',
+    bacharelado: 'bacharelado'
+  } 
   validates :discount_percentage, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
   before_save :calculate_discount_percentage
 
