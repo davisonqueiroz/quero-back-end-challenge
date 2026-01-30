1e4ecdc04b799e909e52ac51c84c5a60fd9a8d33
davison queiroz <davisonvinicius@outlook.com.br>
2026-01-29 21:37:50 -0300

    read and iterate the the json file to create registers in db

--- offers-challenge/db/seeds.rb
+++ offers-challenge/db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+
+file_path = Rails.root.join('db', 'data.json')
+file_data = File.read(file_path)
+json = JSON.parse(file_data)
+offers = json['offers']
+
+offers.each do |offer|
+  Offer.create!(
+    course_name: offer['courseName'],
+    rating: offer['rating'],
+    full_price: offer['fullPrice'],
+    offered_price: offer['offeredPrice'],
+    kind: offer['kind'],
+    level: offer['level'],
+    ies_logo: offer['iesLogo'],
+    ies_name: offer['iesName']
+  )
+end
