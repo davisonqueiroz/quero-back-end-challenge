48e12cfd57b8d28b07a2f21ecc6951f1f9f8f669
davison queiroz <davisonvinicius@outlook.com.br>
2026-01-29 21:13:33 -0300

    added methods for modifying the display of offer values

--- offers-challenge/app/serializers/offer_serializer.rb
+++ offers-challenge/app/serializers/offer_serializer.rb
@@ -1,5 +1,6 @@
 class OfferSerializer < ActiveModel::Serializer
-  attributes :id, :ies_name, :ies_logo, :course_name, :refined_level, :refined_kind
+  attributes :id, :ies_name, :ies_logo, :course_name, :refined_level, :refined_kind, :display_full_price, :display_offered_price, :display_discount_percentage, :rating
+
   LEVELS = {
     'tecnologo' => 'Tecnólogo (graduação)',
     'licenciatura' => 'Licenciatura (graduação)',
@@ -17,6 +18,18 @@ class OfferSerializer < ActiveModel::Serializer
 
   def refined_kind
     KINDS[self.kind]
+  end
+
+  def display_offered_price
+    "R$ #{self.offered_price}"
+  end
+
+  def display_full_price
+    "R$ #{'%.2f' % self.full_price}".sub('.',',')
+  end
+
+  def display_discount_percentage
+    "#{(self.discount_percentage * 100).round}%"
   end
 
 end
